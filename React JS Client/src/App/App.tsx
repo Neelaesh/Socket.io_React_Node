4dd05170d0025e94e904795b881baa2f4eb05599
Neelaesh <neelaesh.kanna_kugan@mercedes-benz.com>
2025-09-01 18:36:52 +0530

    Initial commit
    # Conflicts:
    #	README.md

---
+++ React JS Client/src/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import Box from "@mui/material/Box";
+
+import NotificationCenter from "../components/NotificationCenter/NotificationCenter";
+
+const App: FC = () => {
+  return (
+    <Box>
+      <NotificationCenter />
+    </Box>
+  );
+};
+
+export default App;
